23c5a0d403aae685291ff392d2593e5350b657ee
Gregory Weber <gregory1@weberdao.org>
2023-02-15 12:46:44 -0800

    added listexamples

---
+++ ListExamples.java
@@ -0,0 +1,33 @@
+import java.util.ArrayList;
+import java.util.List;
+
+interface StringChecker { boolean checkString(String s); }
+
+class ListExamples {
+
+  // Returns a new list that has all the elements of the input list for which
+  // the StringChecker returns true, and not the elements that return false, in
+  // the same order they appeared in the input list;
+  static List<String> filter(List<String> list, StringChecker sc) {
+    List<String> result = new ArrayList<>();
+    for(String s: list) {
+      if(sc.checkString(s)) {
+        result.add(s);
+      }
+    }
+    return result;
+  }
+
+
+  // Takes two sorted list of strings (so "a" appears before "b" and so on),
+  // and return a new list that has all the strings in both list in sorted order.
+  static List<String> merge(List<String> list1, List<String> list2) {
+    int count = 0;
+    while (count >= 0) {
+        count++;
+    }
+    return new ArrayList<String>();
+  }
+
+
+}
